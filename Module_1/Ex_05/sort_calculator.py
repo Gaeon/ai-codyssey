6008d55fa73a55cd10d57732f6661bc9415d908e
Gaeon <gaeon0413@gmail.com>
2025-07-15 13:14:28 +0900

    Ex_05

--- Module_1/Ex_05/sort_calculator.py
+++ Module_1/Ex_05/sort_calculator.py
@@ -3,7 +3,16 @@ def main():
 		input_str = input("Enter numbers: ")
 		numbers = [float(n) for n in input_str.split()]
 
-		len = n = len(numbers)
+		len = len(numbers)
+		for i in range(len) :
+			min_index = i
+			for j in range(i + 1, len):
+				if numbers[j] < numbers[min_index]:
+					min_index = j
+			numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+
+		print("Sorted:", " ".join(str(float(num)) for num in numbers))
+
 	except ValueError:
 		print("Invalid input.")
 
